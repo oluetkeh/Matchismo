cb6f5f699f5b9fd75444b762c1a5636873cd8430
Olaf Luetkehoelter <oluetkeh@gmail.com>
2015-08-07 17:32:41 -0400

    restored CardMatchingGame class

---
+++ Matchismo/Model/CardMatchingGame.h
@@ -0,0 +1,13 @@
+//
+//  CardMatchingGame.h
+//  Matchismo
+//
+//  Created by oluetkeh on 8/7/15.
+//  Copyright (c) 2015 Olaf Luetkehoelter. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CardMatchingGame : NSObject
+
+@end
---
+++ Matchismo/Model/CardMatchingGame.m
@@ -0,0 +1,13 @@
+//
+//  CardMatchingGame.m
+//  Matchismo
+//
+//  Created by oluetkeh on 8/7/15.
+//  Copyright (c) 2015 Olaf Luetkehoelter. All rights reserved.
+//
+
+#import "CardMatchingGame.h"
+
+@implementation CardMatchingGame
+
+@end
